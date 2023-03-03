5b3300689b09f6999576363a3b6d067ef43d9392
John Hash <johnhash@MacBookPro.local>
2023-03-03 19:38:37 +0100

    display 3 randoms sentences

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,6 @@ import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  console.log(styles);
   return (
     <Layout>
       <Head>
--- pages/play/index.jsx
+++ pages/play/index.jsx
@@ -1,5 +1,25 @@
+import Layout from "@/components/layout";
+import { useEffect } from "react";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
 export default function Play() {
+  // const { data, error } = useSWR('http://localhost:3001/sentences/getrandoms', fetcher, {refreshInterval: 1000})
+  const { data, error } = useSWR('http://localhost:3001/sentences/getrandoms', fetcher)
+  console.log(data)
+  if (error) return <div>Failed to load</div>
   return (
-    <h1>PLAY</h1>
+    <Layout>
+      <h1>PLAY</h1>
+      {data && 
+        <div>
+          {data.map((sentence) => 
+            <div key={sentence.id}>{sentence.id}</div>
+          )}
+        </div>
+      }
+    </Layout>
+
   )
 }
